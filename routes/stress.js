0167d82698686e6771cec9b96cf44c24add32690
con_mame <con_mame@conma.me>
2015-03-04 17:53:05 +0900

    Fix stress script path

--- routes/stress.js
+++ routes/stress.js
@@ -3,7 +3,7 @@ var exec = require('child_process').exec
 var router = express.Router();
 
 router.get('/', function(req, res) {
-child = exec('timeout 480 sh ../bin/stress &', function(err, stdout, stderr){})
+child = exec('timeout 480 sh ./bin/stress &', function(err, stdout, stderr){})
   res.send('Stress....');
 });
 
